54e71da2ef7e3ff2d2c04e3ada846cc7b8db2fab
DominikaPieRep <dompie6@st.amu.edu.pl>
2017-12-08 16:37:45 +0100

    firs commit, email class and emailapp class has been created, main variables has been added

---
+++ src/emailapp/Email.java
@@ -0,0 +1,23 @@
+package emailapp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	// Constructor to receive the first name and last name
+	
+	// Ask for the departament
+	
+	// Generate a random password
+	
+	// Set  the mailcapacity
+	
+	// Set the alternate email
+	
+	// Change the password
+
+}
---
+++ src/emailapp/Emailapp.java
@@ -0,0 +1,13 @@
+package emailapp;
+
+public class Emailapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
